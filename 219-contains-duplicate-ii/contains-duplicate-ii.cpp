d09263628e5ea1a91b4a2f3f92aa4578c26f0437
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-07-11 12:16:47 +0530

    Time: 78 ms (39.42%) | Memory: 98.8 MB (13.69%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -3,10 +3,8 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int> store;
         for(int i=0;i<nums.size();i++){
-            if(store.count(nums[i])){
-                if(abs(i-store[nums[i]])<=k){
-                    return true;
-                }
+            if(store.count(nums[i]) && abs(i-store[nums[i]])<=k){
+                return true;
             }
             store[nums[i]]=i;
         }
