e17d85013e53138928c753ed90ce8bdeaccc4c89
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-07-15 14:25:41 +0530

    Time: 128 ms (5.26%) | Memory: 149 MB (5.35%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -7,19 +7,13 @@ public:
             int v=edge[1];
 
             adj[u].push_back(v);
-            if(adj[u].size()==edges.size()){
-                return u;
-            }
             adj[v].push_back(u);
-            if(adj[v].size()==edges.size()){
-                return v;
-            }
         }
-        // for(auto &[u,v]:adj){
-        //     if(v.size()==edges.size()){
-        //         return u;
-        //     }          
-        // }
+        for(auto &[u,v]:adj){
+            if(v.size()==edges.size()){
+                return u;
+            }          
+        }
         return -1;
     }
 };
