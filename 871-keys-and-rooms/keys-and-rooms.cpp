6c17778dcf289a4e00efb07383a31fa30b320dda
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-07-15 17:19:25 +0530

    Time: 4 ms (8.27%) | Memory: 15.7 MB (6.69%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void helper(vector<vector<int>>& rooms,set<int> &store,int src){
+        if(store.count(src)){
+            store.erase(src);
+            for(auto v:rooms[src]){
+                if(store.count(v)){
+                    helper(rooms,store,v);
+                }
+            }
+        }
+    }
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        set<int> store;
+        for(int i=0;i<rooms.size();i++){
+            store.insert(i);
+        }
+        helper(rooms,store,0);      
+        if(store.empty()){
+            return true;
+        }
+        return false;
+    }
+};
