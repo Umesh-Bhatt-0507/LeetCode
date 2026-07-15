fb37f0522acfd5f16c06cf4cc5b45be6435ef841
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-07-15 14:41:36 +0530

    Time: 56 ms (21.82%) | Memory: 121.1 MB (22.21%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int,vector<int>> adj;
+        unordered_map<int,int> degree;
         for(auto &edge:edges){
-            int u=edge[0];
-            int v=edge[1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            degree[edge[0]]++;
+            degree[edge[1]]++;
         }
-        for(auto &[u,v]:adj){
-            if(v.size()==edges.size()){
+        for(auto &[u,v]:degree){
+            if(v==edges.size()){
                 return u;
-            }          
+            }
         }
         return -1;
+        
+
+
+        // unordered_map<int,vector<int>> adj;
+        // for(auto &edge:edges){
+        //     int u=edge[0];
+        //     int v=edge[1];
+        //     adj[u].push_back(v);
+        //     adj[v].push_back(u);
+        // }
+        // for(auto &[u,v]:adj){
+        //     if(v.size()==edges.size()){
+        //         return u;
+        //     }          
+        // }
+        // return -1;
     }
 };
