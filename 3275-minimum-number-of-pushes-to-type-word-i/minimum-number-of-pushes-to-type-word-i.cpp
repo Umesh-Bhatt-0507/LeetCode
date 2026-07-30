745f6db084df06a5a6e4dd973aa694f3b13fdc64
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-07-30 14:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (95.83%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int len=word.size();
-        int count=0;
-        int i=1;
-        while(len>0){
-            count+=min(len,8)*i;
-            i++;
-            len=len-8;
+        int size=word.size();
+        int rem=size%8;
+        int que=size/8;
+        rem*=(que+1);
+        int mul=0;
+        while(que!=0){
+            mul+=que;
+            que--;
         }
-        return count;
+        return 8*(mul)+rem;
+        
     }
 };
