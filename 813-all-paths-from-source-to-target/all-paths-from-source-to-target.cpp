a08f99db891dda7415c42d9c78106e41cce09a53
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-08-08 23:44:03 +0530

    Time: 15 ms (20.81%) | Memory: 20.9 MB (10.12%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     vector<vector<int>> answer;
     void dfs(vector<vector<int>>& graph,int u,vector<int> store){
+        store.push_back(u);
         if(u==(graph.size()-1)){
-            store.push_back(u);
             answer.push_back(store);
             return;
         }
-        store.push_back(u);
         for(auto &v:graph[u]){
             dfs(graph,v,store);
         }
