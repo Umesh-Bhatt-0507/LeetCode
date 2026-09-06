eb69a04f2bb4ef4a148f4242db841d651a315392
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-09-06 15:03:03 +0530

    Time: 167 ms (67.62%) | Memory: 182.9 MB (43.30%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    int time;
-    vector<int> dt,low;
-    void dfs(int u,int parU,vector<vector<int>> &cc,vector<vector<int>> &graph){
+    vector<int> dt;
+    vector<int> low;
+    int time=0;
+    vector<vector<int>> answer;
+    void dfs(int u,int parU,vector<vector<int>>& connections){
         dt[u]=low[u]=++time;
-        for(auto &v:graph[u]){
+        for(auto &v:connections[u]){
             if(dt[v]==-1){
-                dfs(v,u,cc,graph);
+                dfs(v,u,connections);
                 low[u]=min(low[u],low[v]);
                 if(low[v] > dt[u]){
-                    cc.push_back({u,v});
+                    answer.push_back({u,v});
                 }
             }else if(v!=parU){
                 low[u]=min(low[u],dt[v]);
             }
         }
     }
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections){
-        vector<vector<int>> bridges;
-        vector<vector<int>> graph(n);
-        for(auto u:connections){
-            graph[u[0]].push_back(u[1]);
-            graph[u[1]].push_back(u[0]);
-        }
-        
-        time=0;
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         dt.resize(n,-1);
         low.resize(n);
+        vector<vector<int>> adj(n);
+        for(auto &e:connections){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
         for(int i=0;i<n;i++){
             if(dt[i]==-1){
-                dfs(i,-1,bridges,graph);
+                dfs(i,-1,adj);
             }
         }
-        return bridges;
+        return answer;
     }
 };
