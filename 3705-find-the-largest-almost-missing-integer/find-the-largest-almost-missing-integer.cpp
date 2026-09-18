555061aa9a20cccc67a5e83f3269a66c80da5bc8
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-09-18 19:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (98.22%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -1,23 +1,69 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        unordered_set<int> s;
-        for(int i=0;i<=nums.size()-k;i++){
-            for(int j=i;j<k+i;j++){
-                if(!s.count(nums[j])){
-                    mp[nums[j]]++;
+        int n=nums.size();
+        if(k==1){
+            sort(nums.begin(),nums.end());
+            for(int i=n-1;i>=0;i--){
+                if(i==0){
+                    return nums[i];
                 }
-                s.insert(nums[j]);
+                if(nums[i]!=nums[i-1]){
+                    return nums[i];
+                }
+                while(i>0 && nums[i]==nums[i-1]){
+                    i--;
+                }
             }
-            s.clear();
+            return -1;
         }
-        int m=-1;
-        for(auto &v: mp){
-            if(v.second==1){
-                m=max(m,v.first);
+        if(k==n){
+            int m=INT_MIN;
+            for(int i=0;i<n;i++){
+                m=max(m,nums[i]);
+            }
+            return m;
+        }
+        int a=nums[0];
+        int b=nums[n-1];
+        if(a==b){
+            return -1;
+        }
+        bool ansa=false;
+        bool ansb=false;
+        for(int i=1;i<n-1;i++){
+            if(nums[i]==a){
+                ansa=true;
+            }
+            if(nums[i]==b){
+                ansb=true;
             }
         }
-        return m;
+        if(ansa && ansb){
+            return -1;
+        }else  if(ansa && !ansb){
+            return b;
+        }else if(ansb && !ansa){
+            return a;
+        }
+        return max(a,b);
+        // unordered_map<int,int> mp;
+        // unordered_set<int> s;
+        // for(int i=0;i<=nums.size()-k;i++){
+        //     for(int j=i;j<k+i;j++){
+        //         if(!s.count(nums[j])){
+        //             mp[nums[j]]++;
+        //         }
+        //         s.insert(nums[j]);
+        //     }
+        //     s.clear();
+        // }
+        // int m=-1;
+        // for(auto &v: mp){
+        //     if(v.second==1){
+        //         m=max(m,v.first);
+        //     }
+        // }
+        // return m;
     }
 };
