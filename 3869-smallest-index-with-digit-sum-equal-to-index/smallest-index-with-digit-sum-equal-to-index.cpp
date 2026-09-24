c37564ac28c2cf34daccd32d657afa781c942ae2
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-09-24 15:03:17 +0530

    Time: 0 ms (100.00%) | Memory: 30.9 MB (81.66%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -10,7 +10,10 @@ public:
     }
     int smallestIndex(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
-            if(i==calSum(nums[i])){
+            if(nums[i]<10 && i==nums[i]){
+                return i;
+            }
+            else if(i==calSum(nums[i])){
                 return i;
             }
         }
