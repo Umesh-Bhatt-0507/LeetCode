7a8ad418967011d56cb7af0bb7a4f9d3515486cd
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-09-25 12:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.39%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -8,7 +8,15 @@ public:
         return dp[n]=climbStairsMemo(n-1,dp)+climbStairsMemo(n-2,dp);
     }
     int climbStairs(int n) {
+        if(n<=2) return n;
         vector<int> dp(n+1,-1);
-        return climbStairsMemo(n,dp);
+        // return climbStairsMemo(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
