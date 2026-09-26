08022550a36bbd02f3817a43b0ebf0f774d115fd
UMESH BHATT <128054094+Umesh-Bhatt-0507@users.noreply.github.com>
2026-09-26 12:12:44 +0530

    Time: 101 ms (85.21%) | Memory: 142.1 MB (40.00%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        // unordered_map<int,int> store;
-        // for(int i=0;i<nums.size();i++){
-        //     if(store.count(nums[i]) && abs(i-store[nums[i]])<=k){
-        //         return true;
-        //     }
-        //     store[nums[i]]=i;
-        // }
-        // return false;
-
-        unordered_set<int> s;
-        int r=0;
-        int l=0;
+        unordered_map<int,int> store;
         for(int i=0;i<nums.size();i++){
-            if(r-l > k){
-                s.erase(nums[i-k-1]);
-                l++;
-            }
-            if(s.count(nums[i])){
+            if(store.count(nums[i]) && abs(i-store[nums[i]])<=k){
                 return true;
             }
-            s.insert(nums[i]);
-            r++;
+            store[nums[i]]=i;
         }
         return false;
+
+        // unordered_set<int> s;
+        // int r=0;
+        // int l=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(r-l > k){
+        //         s.erase(nums[i-k-1]);
+        //         l++;
+        //     }
+        //     if(s.count(nums[i])){
+        //         return true;
+        //     }
+        //     s.insert(nums[i]);
+        //     r++;
+        // }
+        // return false;
     }
 };
